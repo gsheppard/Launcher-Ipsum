06e8f9d4dc88436196734e233d51b1c004ca4f0b
Gregory Sheppard <gregory.sheppard@live.com>
2014-02-21 12:55:20 -0500

    Moved the dictionary population to its own file

--- launch_ipsum.rb
+++ launch_ipsum.rb
@@ -1,23 +1,2 @@
 require 'pry'
 
-alpha = 'a'
-dictionary_filename = "pages/dictionary.md"
-
-File.open(dictionary_filename, "w") do |dict_file|
-	while alpha != 'z'.next
-		File.open("pages/nasa_#{alpha}.html").each do |line|
-			line = line.encode("US-ASCII", :invalid => :replace, :undef => :replace, :replace => "?")
-
-			if line.include?("<DT><STRONG>")
-				line.gsub!(/\<DT\>\<STRONG\>/, '')
-				line.gsub!(/(\<\/STRONG\>).*/, '')
-				line.gsub!(/\(.*\).*/, '')
-				line.gsub!(/^\s*/, '')
-				line.gsub!(/[^a-zA-Z0-9\s]/, "")
-				dict_file.write line
-			end
-		end
-
-		alpha.next!
-	end
-end
