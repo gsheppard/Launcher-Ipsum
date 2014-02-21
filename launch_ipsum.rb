4cf1dbb520348319ada69869ee5f6437d9e03322
Gregory Sheppard <gregory.sheppard@live.com>
2014-02-21 12:53:01 -0500

    Added UTF-8 to ASCII conversion on a per line basis

--- launch_ipsum.rb
+++ launch_ipsum.rb
@@ -1,12 +1,23 @@
 require 'pry'
 
-dictionary_filename = "pages/dictionary.txt"
+alpha = 'a'
+dictionary_filename = "pages/dictionary.md"
 
 File.open(dictionary_filename, "w") do |dict_file|
-	File.open("pages/nasa_a.html").each do |line|
-		# binding.pry
-		if line.include?("<DT><STRONG>")
-			dict_file.write line
+	while alpha != 'z'.next
+		File.open("pages/nasa_#{alpha}.html").each do |line|
+			line = line.encode("US-ASCII", :invalid => :replace, :undef => :replace, :replace => "?")
+
+			if line.include?("<DT><STRONG>")
+				line.gsub!(/\<DT\>\<STRONG\>/, '')
+				line.gsub!(/(\<\/STRONG\>).*/, '')
+				line.gsub!(/\(.*\).*/, '')
+				line.gsub!(/^\s*/, '')
+				line.gsub!(/[^a-zA-Z0-9\s]/, "")
+				dict_file.write line
+			end
 		end
+
+		alpha.next!
 	end
 end
