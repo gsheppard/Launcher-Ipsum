2913545a256234a2bdb6ab2196f535a35811d070
Gregory Sheppard <gregory.sheppard@live.com>
2014-02-23 18:55:22 -0500

    Updated for file validation.

--- launch_ipsum.rb
+++ launch_ipsum.rb
@@ -12,7 +12,7 @@ def valid? answer
 	answer.match(/\d/) ? true : false
 end
 
-def get_paragraph2 lines
+def get_paragraph lines
 	sent_per_paragraph = rand(4..12)
 	sent_ctr = 0
 	single_paragraph = []
@@ -34,6 +34,7 @@ def get_paragraph2 lines
 			words_ctr += 1
 		end
 
+		sentence[0].capitalize!
 		single_paragraph << sentence
 		sent_ctr += 1
 	end
@@ -41,18 +42,39 @@ def get_paragraph2 lines
 	single_paragraph
 end
 
-# ----------------------------
 # ------ Method Barrier ------
-# ----------------------------
 
 puts "Launcher Ipsum"
-ipsum = []
 
+# Checks for dictionary
+unless File.exist?("dictionary.md")
+	puts ">> Dictionary doesn't exist!"
+
+	unless File.exist?("dictionary_populate.rb")
+		puts ">> Necessary files to build dictionary are not present."
+		puts "Exiting..."
+		exit
+	end
+
+	print ">> Build dictionary? (Y/n) "
+	build = gets.chomp
+
+	if build == ('y' || 'Y')
+		load 'dictionary_populate.rb'
+		populate()
+	else
+		puts "Exiting..."
+		exit
+	end
+end
+
+ipsum = []
 lines = get_lines()
 
 print "How many paragraphs do you need? (1-20) "
 num_paragraphs = gets.chomp
 
+# entry validation
 while num_paragraphs.to_i > 20 || num_paragraphs.to_i < 1 || !valid?(num_paragraphs)
 	puts ">> That's not valid."
 	print "How many paragraphs (within reason) do you need? (1-20) "
@@ -64,11 +86,11 @@ puts "---------------------------------"
 # Build paragraphs
 loop_ctr = 0
 while loop_ctr < num_paragraphs.to_i
-	ipsum << get_paragraph2(lines)
+	ipsum << get_paragraph(lines)
 	loop_ctr += 1
 end
 
-
+# Output
 ipsum.each do |para|
 	print "\t"
 	para.each do |sent|
