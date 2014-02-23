625a7627b885485d68942d656701803b8a417790
Gregory Sheppard <gregory.sheppard@live.com>
2014-02-23 18:11:20 -0500

    Due to the depedency of get_paragraph to get_sentence, I combined them into the same method (get_paragraph).

--- launch_ipsum.rb
+++ launch_ipsum.rb
@@ -12,34 +12,29 @@ def valid? answer
 	answer.match(/\d/) ? true : false
 end
 
-def get_sentence lines
-	words_per_sentence = rand(5..10)
-	words_ctr = 0
-	sentence = []
-
-	while words_ctr < words_per_sentence
-		random_line = ":" + rand(lines).to_s + ":" # returns line number in format of 123:
-
-		File.open("dictionary.md").each do |line|
-			if line.include?(random_line)
-				sentence << line.gsub(/(\:\d*\:\s)/, '').chomp
-			end
-		end
-
-		words_ctr += 1
-	end
-
-	sentence[0].capitalize!
-	sentence
-end
-
-def get_paragraph lines # dependent on get_sentence
+def get_paragraph2 lines
 	sent_per_paragraph = rand(4..12)
 	sent_ctr = 0
 	single_paragraph = []
 
-	while sent_per_paragraph > sent_ctr
-		single_paragraph << get_sentence(lines)
+	while sent_ctr < sent_per_paragraph
+		words_per_sentence = rand(5..10)
+		words_ctr = 0
+		sentence = []
+
+		while words_ctr < words_per_sentence
+			random_line = ":" + rand(lines).to_s + ":" # returns line number in format of 123:
+
+			File.open("dictionary.md").each do |line|
+				if line.include?(random_line)
+					sentence << line.gsub(/(\:\d*\:\s)/, '').chomp
+				end
+			end
+
+			words_ctr += 1
+		end
+
+		single_paragraph << sentence
 		sent_ctr += 1
 	end
 
@@ -69,7 +64,7 @@ puts "---------------------------------"
 # Build paragraphs
 loop_ctr = 0
 while loop_ctr < num_paragraphs.to_i
-	ipsum << get_paragraph(lines)
+	ipsum << get_paragraph2(lines)
 	loop_ctr += 1
 end
 
